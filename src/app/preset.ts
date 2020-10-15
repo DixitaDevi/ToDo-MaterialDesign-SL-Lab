79524226fb41a7bf642c667e8082a0500fb3af58
Dixita-10 <56042250+Dixita-10@users.noreply.github.com>
2020-10-15 18:49:39 +0530

    commited

---
+++ src/app/preset.ts
@@ -0,0 +1,7 @@
+import {Todo} from './todo';
+export const Todos: Todo[] = [
+{ id : 1, date : '12/1/2020', title : 'Morning walk', description : 'Walk for at least an hour'},
+{ id : 2, date : '12/2/2020', title : 'Gardening', description : 'Do plant the new seeds'},
+{ id : 3, date : '12/3/2020', title : 'Coding', description : 'Learn Golang Today'},
+{ id : 4, date : '12/4/2020', title : 'Submit assignment', description : 'Submit your SL Assignment'}
+];
